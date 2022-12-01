b239a3b7f34d5a08dced57f535a22c12db23f691
Pragati-patel <peehupatel1998@gmail.com>
2022-12-01 22:52:00 +0530

    merge code and fixed upper footer

--- components/footer/UpperFooter/index.js
+++ components/footer/UpperFooter/index.js
@@ -67,7 +67,7 @@ export default function UpperFooter() {
               <div key={index} className="flex flex-col">
                 <h1>{i.heading}</h1>
                 {i.list.map((i, index) => {
-                  return <span key={index}>{i}</span>;
+                  return <span key={index} className={styles.links}>{i}</span>;
                 })}
               </div>
             );
