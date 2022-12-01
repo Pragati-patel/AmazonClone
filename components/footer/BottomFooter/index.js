59db31f022c72dc2c3ee977f2d8ad9882025a085
Souvik Mandal <thedevsouvik@gmail.com>
2022-12-01 14:13:42 +0000

    Update 22 files

--- components/footer/BottomFooter/index.js
+++ components/footer/BottomFooter/index.js
@@ -1,49 +1,90 @@
-import styles from './style.module.scss'
+import styles from "./style.module.scss";
 export default function BottomFooter() {
-    const bottomLinks = [
-        { "item": "Conditions of use & sale" },
-        { "item": "Privacy Notice" },
-        { "item": "Interest-Based Ads" },
-    ]
-    const linksData = [
-        { "ui1_li1": "AbeBooks", "ui1_li2": "Books, art", "ui1_li3": "& collectibles", 'ui2_li1': "Shopbop", 'ui2_li2': "Designer", 'ui2_li3': 'Fashion Brands' },
-        { "ui1_li1": "Amazon Web Services", "ui1_li2": "Scalable Cloud", "ui1_li3": "Computing Services", 'ui2_li1': "Amazon Business", 'ui2_li2': "Everything For", 'ui2_li3': 'Your Business' },
-        { "ui1_li1": "Audible", "ui1_li2": "Download", "ui1_li3": "Audio Books", 'ui2_li1': "	Prime Now", 'ui2_li2': "2-Hour Delivery", 'ui2_li3': 'on Everyday Items' },
-        { "ui1_li1": "DPReview", "ui1_li2": "Digital", "ui1_li3": "Photography", 'ui2_li1': "Amazon Prime Music", 'ui2_li2': "90 million songs, ad-free", 'ui2_li3': 'Over 15 million podcast episodes' },
-        { "ui1_li1": "	IMDb", "ui1_li2": "Movies, TV", "ui1_li3": "& Celebrities", 'ui2_li1': "", 'ui2_li2': "", 'ui2_li3': '' }
-    ]
-    
-    return (<div className={styles.bottom_footer_wrapper}>
-        {/* //links data  */}
-        <div className={styles.bottom_footer_inner}>
-            <div className=' flex justify-between'>
-                {linksData.map((item) => {
-                    return (
+  const bottomLinks = [
+    { item: "Conditions of use & sale" },
+    { item: "Privacy Notice" },
+    { item: "Interest-Based Ads" },
+  ];
+  const linksData = [
+    {
+      ui1_li1: "AbeBooks",
+      ui1_li2: "Books, art",
+      ui1_li3: "& collectibles",
+      ui2_li1: "Shopbop",
+      ui2_li2: "Designer",
+      ui2_li3: "Fashion Brands",
+    },
+    {
+      ui1_li1: "Amazon Web Services",
+      ui1_li2: "Scalable Cloud",
+      ui1_li3: "Computing Services",
+      ui2_li1: "Amazon Business",
+      ui2_li2: "Everything For",
+      ui2_li3: "Your Business",
+    },
+    {
+      ui1_li1: "Audible",
+      ui1_li2: "Download",
+      ui1_li3: "Audio Books",
+      ui2_li1: "	Prime Now",
+      ui2_li2: "2-Hour Delivery",
+      ui2_li3: "on Everyday Items",
+    },
+    {
+      ui1_li1: "DPReview",
+      ui1_li2: "Digital",
+      ui1_li3: "Photography",
+      ui2_li1: "Amazon Prime Music",
+      ui2_li2: "90 million songs, ad-free",
+      ui2_li3: "Over 15 million podcast episodes",
+    },
+    {
+      ui1_li1: "	IMDb",
+      ui1_li2: "Movies, TV",
+      ui1_li3: "& Celebrities",
+      ui2_li1: "",
+      ui2_li2: "",
+      ui2_li3: "",
+    },
+  ];
 
-                        <div className={styles.link_box_wrap}>
-                            <ul className='flex flex-col mb-4 hover:cursor-pointer hover:underline hover:decoration-gray-300'>
-                                <li className='text-gray-300 text-11px font-AmazonEmberMedium'>{item.ui1_li1}</li>
-                                <li className='text-gray-400 text-10px '>{item.ui1_li2}</li>
-                                <li className='text-gray-400 text-10px'>{item.ui1_li3}</li>
-                            </ul>
-                            <ul className='flex flex-col hover:cursor-pointer hover:underline hover:decoration-gray-300'>
-                                <li className='text-gray-300 text-11px font-AmazonEmberMedium'>{item.ui2_li1}</li>
-                                <li className='text-gray-400 text-10px'>{item.ui2_li2}</li>
-                                <li className='text-gray-400 text-10px'>{item.ui2_li3}</li>
-                            </ul>
-                        </div>
-                    )
-
-                })}
-            </div>
-
-            {/* //most bottom footer  */}
-            <div className={styles.bottom_footer_links}>
-                <ul className='flex'>
-                    {bottomLinks.map((i) => (<li>{i.item}</li>))}
+  return (
+    <div className={styles.bottom_footer_wrapper}>
+      {/* //links data  */}
+      <div className={styles.bottom_footer_inner}>
+        <div className=" flex justify-between">
+          {linksData.map((item, index) => {
+            return (
+              <div key={index} className={styles.link_box_wrap}>
+                <ul className="flex flex-col mb-4 hover:cursor-pointer hover:underline hover:decoration-gray-300">
+                  <li className="text-gray-300 text-11px font-AmazonEmberMedium">
+                    {item.ui1_li1}
+                  </li>
+                  <li className="text-gray-400 text-10px ">{item.ui1_li2}</li>
+                  <li className="text-gray-400 text-10px">{item.ui1_li3}</li>
                 </ul>
-                <p>© 1996-2022, Amazon.com, Inc. or its affiliates</p>
-            </div>
+                <ul className="flex flex-col hover:cursor-pointer hover:underline hover:decoration-gray-300">
+                  <li className="text-gray-300 text-11px font-AmazonEmberMedium">
+                    {item.ui2_li1}
+                  </li>
+                  <li className="text-gray-400 text-10px">{item.ui2_li2}</li>
+                  <li className="text-gray-400 text-10px">{item.ui2_li3}</li>
+                </ul>
+              </div>
+            );
+          })}
         </div>
-    </div>)
+
+        {/* //most bottom footer  */}
+        <div className={styles.bottom_footer_links}>
+          <ul className="flex">
+            {bottomLinks.map((i, index) => (
+              <li key={index}>{i.item}</li>
+            ))}
+          </ul>
+          <p>© 1996-2022, Amazon.com, Inc. or its affiliates</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/header/MegaMenu/index.js
+++ components/header/MegaMenu/index.js
@@ -1,12 +1,35 @@
 export default function MegaMenu() {
-    const megaMenuList = ['Amazon Home','Kitchen & Home Appliances', 'Large Appliances', 'Kitchen & Dining', 'Furniture', 'Home Furnishing', 'Home Decor', 'Home improvement', 'Garden & Outdoor', 'Storage & organisaton', 'Lighting']
-    return (
-        <div className="flex justify-evenly items-center w-full" style={{boxShadow:"0 0 1px #ddd"}}>
-            <ul className="flex justify-evenly w-full">
-                {megaMenuList.map((i)=>{
-                    return <li className="p-3 list-none text-12px font-AmazonEmberMedium" style={{color:"#333"}}>{i}</li>
-                })}
-            </ul>
-        </div>
-    )
+  const megaMenuList = [
+    "Amazon Home",
+    "Kitchen & Home Appliances",
+    "Large Appliances",
+    "Kitchen & Dining",
+    "Furniture",
+    "Home Furnishing",
+    "Home Decor",
+    "Home improvement",
+    "Garden & Outdoor",
+    "Storage & organisaton",
+    "Lighting",
+  ];
+  return (
+    <div
+      className="flex justify-evenly items-center w-full"
+      style={{ boxShadow: "0 0 1px #ddd" }}
+    >
+      <ul className="flex justify-evenly w-full">
+        {megaMenuList.map((i, index) => {
+          return (
+            <li
+              key={index}
+              className="p-3 list-none text-12px font-AmazonEmberMedium"
+              style={{ color: "#333" }}
+            >
+              {i}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
