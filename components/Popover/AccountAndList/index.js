5794d3aedb8ef95ecf4eb84d77b34d46ed9c8a37
Pragati-patel <peehupatel1998@gmail.com>
2022-12-02 14:41:19 +0530

    add new popover that is AccountAndList

---
+++ components/Popover/AccountAndList/index.js
@@ -0,0 +1,8 @@
+import styles from './style.module.scss'
+export default function AccountAndList (){
+    return(
+        <div className={styles.account_popup_wrapper}>
+            dfhsdgf
+        </div>
+    )
+}
