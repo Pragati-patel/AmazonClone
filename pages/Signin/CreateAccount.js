e350ebbd673d86d226f7b2a2b5054077329c0a1b
Pragati-patel <peehupatel1998@gmail.com>
2022-12-28 23:55:09 +0530

    set showSignin state from child component (createAccount) and put hover effect on continue button

--- pages/Signin/CreateAccount.js
+++ pages/Signin/CreateAccount.js
@@ -6,8 +6,7 @@ import { BsInfo } from 'react-icons/bs'
 import { AiFillCaretRight } from 'react-icons/ai'
 import { useEffect } from 'react';
 
-export default function CreateAccount(showSignin) {
-    useEffect(()=>{console.log(showSignin,"propsss")},[])
+export default function CreateAccount({ control }) {
     return (
         <div className={styles.signin_box}>
             <h1 className={styles.signin_heading}>Create Account</h1>
@@ -27,21 +26,28 @@ export default function CreateAccount(showSignin) {
                 {strings.create_account.password}
             </label>
             <input type='password' className={styles.singin_input} placeholder="At least 6 characters" />
-            <p className='flex text-12px items-center'><BsInfo color='#0066c0' size={'1.2rem'} />Passwords must be at least 6 characters</p>
+
+            <p className='flex text-12px items-center'><BsInfo color='#0066c0' size={'1.2rem'} />
+                Passwords must be at least 6 characters</p>
 
             <p className='text-12px my-2  '>{strings.create_account.info}</p>
             <button className={styles.continue_btn}>{strings.Continue}</button>
             <p className={styles.create_acc_info}>
-                Already have an account?  <span onClick={()=>{showSignin()}}>Sign in<AiFillCaretRight size={"0.5rem"} className={styles.downArrowIcon} /> </span></p>
-            <p className={styles.create_acc_info} style={{marginTop:"0"}}>Buying for work?
+                Already have an account?
+                <span onClick={() => { control(true) }}>
+                    Sign in
+                    <AiFillCaretRight size={"0.5rem"} className={styles.downArrowIcon} />
+                </span>
+            </p>
+            <p className={styles.create_acc_info} style={{ marginTop: "0" }}>Buying for work?
                 <span>
-                    Create a free business account<AiFillCaretRight size={"0.5rem"} className={styles.downArrowIcon} />  </span></p>
-
-                    
-            <p className='text-12px my-2'>By continuing, you agree to Amazon's
-                <span className='text-link-secondary'> Conditions of Use </span>
+                    Create a free business account<AiFillCaretRight size={"0.5rem"} className={styles.downArrowIcon} />  </span>
+            </p>
+            <p className='text-12px my-2 mt-4'>By continuing, you agree to Amazon's
+                <span className={styles.span_link}> Conditions of Use </span>
                 and
-                <span className='text-link-secondary'>Privacy Notice.</span></p>
+                <span className={styles.span_link}>Privacy Notice.</span>
+            </p>
 
         </div>
     )
