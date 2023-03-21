367ee9543f0fc82c2fb8178bf77f796afb05d8ac
Pragati-patel <peehupatel1998@gmail.com>
2023-03-21 11:14:31 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
       <MenuHeader />
       {/* <MegaMenu/> */}
       <BannerCarousel/>
-      <CategoryCard/>
+      {/* <CategoryCard/> */}
       <CategorySection/>
       <ImagesMultiCarousel/>
       <MultiCarousel/>
